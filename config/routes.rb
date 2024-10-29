48d166916008e32a483d5cda4314bac369df3e3c
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2024-10-29 21:43:45 +0700

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'transactions/by_month', to: 'transactions#by_month'
+  get 'transactions/by_date_range', to: 'transactions#by_date_range'
 end
