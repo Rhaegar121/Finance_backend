3d599e4b929839d1a52dead8d3fbf833341aaef2
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2024-11-02 12:00:09 +0700

    add route for yearly data

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'transactions/filter', to: 'transactions#filter'
   get 'transactions/search', to: 'transactions#search'
+  get 'transactions/yearly_summary', to: 'transactions#yearly_summary'
 
   resources :transactions
 end
