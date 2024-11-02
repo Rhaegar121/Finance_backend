ef0c061256a05ffce927411d9fccd49b2a190140
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2024-11-02 12:00:28 +0700

    add method for yearly data

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -60,7 +60,19 @@ class TransactionsController < ApplicationController
     end
   end
 
+  def yearly_summary
+    start_date, end_date = determine_date_range
+    excluded_categories = params[:categories] || []
+
+    # Prepare queries to handle income and expenses
+    @monthly_income = calculate_monthly_totals(start_date, end_date, true, excluded_categories)
+    @monthly_expenses = calculate_monthly_totals(start_date, end_date, false, excluded_categories)
+
+    render json: { income: @monthly_income, expenses: @monthly_expenses }
+  end
+
   private
+
   def transaction_params
     params.require(:transaction).permit(:date, :amount, :description, :category, :income)
   end
@@ -69,5 +81,30 @@ class TransactionsController < ApplicationController
     @transaction = Transaction.find(params[:id])
   rescue ActiveRecord::RecordNotFound
     render json: { error: 'Transaction not found' }, status: :not_found
+  end
+
+  def calculate_monthly_totals(start_date, end_date, is_income, excluded_categories)
+    Transaction.where(date: start_date..end_date, income: is_income)
+               .where.not(category: excluded_categories)
+               .group(Arel.sql("DATE_TRUNC('month', date)"))
+               .order(Arel.sql("DATE_TRUNC('month', date)"))
+               .sum(:amount)
+  end
+
+  def determine_date_range
+    if params[:start_date].present? && params[:end_date].present?
+      start_date = Date.strptime(params[:start_date], "%B %Y").beginning_of_month
+      end_date = Date.strptime(params[:end_date], "%B %Y").end_of_month
+      [start_date, end_date]
+    else
+      # If no dates are provided, return data for all available months in the current year
+      current_year = Date.current.year
+      first_transaction_date = Transaction.minimum(:date) || Date.current.beginning_of_year
+      start_date = [first_transaction_date, Date.current.beginning_of_year].max
+      end_date = [Transaction.maximum(:date) || Date.current.end_of_year, Date.current.end_of_year].min
+      [start_date, end_date]
+    end
+  rescue ArgumentError
+    render json: { error: 'Invalid date format' }, status: :bad_request
   end
 end
